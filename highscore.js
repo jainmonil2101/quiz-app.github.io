886d87751d6a7050f1856d4722d634b16bc93f3e
Monil <jainmonil2101@gmail.com>
2021-04-02 14:23:04 +0530

    Quiz App Using JavaScript

---
+++ highscore.js
@@ -0,0 +1,10 @@
+const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScores
+  .map((score) => {
+    return `<li class="high-score">${score.name[0].toUpperCase() + score.name.slice(1, )} - ${score.score}</li>`;
+  })
+  .join("");
+
+// console.log(score.name[0].toUpperCase() + score.name.slice(1, ));
